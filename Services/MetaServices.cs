9e20daa2d110fb2af5df46163d414f77a0bf0d2c
BrynOvalles <brynovallesburgos@gmail.com>
2024-02-12 19:03:47 -0400

    Creando el Services

---
+++ Services/MetaServices.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using parcial1_Ap1_BryanOvalles.DAL;
+using parcial1_Ap1_BryanOvalles.Models;
+
+namespace parcial1_Ap1_BryanOvalles.Services;
+
+public class MetaServices
+{
+	private readonly Contexto _contexto;
+	public MetaServices(Contexto contexto)
+	{
+		_contexto = contexto;
+	}
+	public async Task<bool> Existe(int id)
+	{
+		return await _contexto.metas.AnyAsync(m => m.MetaId == id);
+	}
+	public async Task<bool> Crear(Metas meta)
+	{
+		if (!await Existe(meta.MetaId))
+			return await Insertar(meta);
+		else
+			return await Modificar(meta);
+	}
+	public async Task<bool> Insertar(Metas meta)
+	{
+		_contexto.metas.Add(meta);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
+	public async Task<bool> Modificar(Metas meta)
+	{
+		_contexto.metas.Update(meta);
+		var modificado = await _contexto.SaveChangesAsync() > 0;
+		_contexto.Entry(meta).State = EntityState.Detached;
+		return modificado;
+	}
+	public async Task<bool> Eliminar(Metas metas)
+	{
+		return await _contexto.metas
+			.AsNoTracking()
+			.Where(m => m.MetaId == metas.MetaId)
+			.ExecuteDeleteAsync() > 0;
+	}
+	public async Task<Metas?> Buscar(int id)
+	{
+		return await _contexto.metas
+			.AsNoTracking()
+			.FirstOrDefaultAsync(m => m.MetaId == id);
+	}
+	public List<Metas> ObtenerLista(Expression<Func<Metas, bool>> criterio)
+	{
+		return _contexto.metas.AsNoTracking()
+			.Where(criterio)
+			.ToList();
+	}
+}
