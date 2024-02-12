b0cc1a0be15743755a1a361c5699c4736b6bfc0e
BrynOvalles <brynovallesburgos@gmail.com>
2024-02-12 18:20:04 -0400

    Creando el modelo de metas

---
+++ Models/Metas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace parcial1_Ap1_BryanOvalles.Models
+{
+	public class Metas
+	{
+		[Key]
+		public int MetaId { get; set; }
+		[Required(ErrorMessage = "Es requerido intruducir una fecha")]
+		public DateOnly Fecha { get; set; } = new DateOnly();
+		[Required(ErrorMessage = "Es requerido que tenga una descripción")]
+		public string? descripcion { get; set; }
+		[Required(ErrorMessage ="Es requerido que ingreses un monto para tu meta")]
+		public decimal monto { get; set; }
+
+	}
+}
