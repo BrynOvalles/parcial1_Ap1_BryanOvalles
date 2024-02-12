893d35282b76a7f64f7f4098014719ed7d28d46a
BrynOvalles <brynovallesburgos@gmail.com>
2024-02-12 18:26:18 -0400

    Creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using parcial1_Ap1_BryanOvalles.Models;
+
+namespace parcial1_Ap1_BryanOvalles.DAL;
+
+public class Contexto : DbContext
+{
+	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+	public DbSet<Metas> metas { get; set; }
+}
