8a907a9dc52a7512bf3f877f3eb36eb42e07e412
BrynOvalles <brynovallesburgos@gmail.com>
2024-02-12 20:49:15 -0400

    Agregando Pequeñas mejoras

--- Services/MetaServices.cs
+++ Services/MetaServices.cs
@@ -14,7 +14,7 @@ public class MetaServices
 	}
 	public async Task<bool> Existe(int id)
 	{
-		return await _contexto.metas.AnyAsync(m => m.MetaId == id);
+		return await _contexto.Metas.AnyAsync(m => m.MetaId == id);
 	}
 	public async Task<bool> Crear(Metas meta)
 	{
@@ -25,32 +25,38 @@ public class MetaServices
 	}
 	public async Task<bool> Insertar(Metas meta)
 	{
-		_contexto.metas.Add(meta);
+		_contexto.Metas.Add(meta);
 		return await _contexto.SaveChangesAsync() > 0;
 	}
 	public async Task<bool> Modificar(Metas meta)
 	{
-		_contexto.metas.Update(meta);
+		_contexto.Metas.Update(meta);
 		var modificado = await _contexto.SaveChangesAsync() > 0;
 		_contexto.Entry(meta).State = EntityState.Detached;
 		return modificado;
 	}
 	public async Task<bool> Eliminar(Metas metas)
 	{
-		return await _contexto.metas
+		return await _contexto.Metas
 			.AsNoTracking()
 			.Where(m => m.MetaId == metas.MetaId)
 			.ExecuteDeleteAsync() > 0;
 	}
 	public async Task<Metas?> Buscar(int id)
 	{
-		return await _contexto.metas
+		return await _contexto.Metas
 			.AsNoTracking()
 			.FirstOrDefaultAsync(m => m.MetaId == id);
 	}
+	public async Task<Metas?> BuscarDescripcion(string descripcion)
+	{
+		return await _contexto.Metas
+			.AsNoTracking()
+			.FirstOrDefaultAsync(m => m.Descripcion == descripcion);
+	}
 	public List<Metas> ObtenerLista(Expression<Func<Metas, bool>> criterio)
 	{
-		return _contexto.metas.AsNoTracking()
+		return _contexto.Metas.AsNoTracking()
 			.Where(criterio)
 			.ToList();
 	}
